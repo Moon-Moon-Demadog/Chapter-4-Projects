dd46a5dafb3316f193e8f24a77153b426f557f9f
Moon-Moon-Demadog <lmw2385@email.vccs.edu>
2025-07-05 01:22:37 +0000

    writing code
    writing code for option 1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 // This program will allow user to calculate basic geometry formulas.
 #include <iostream>
 #include <cmath>
+#include <iomanip>
+using namespace std;
 
 int main()
 {
+  int choice;
+  cout<< "Lets factor some Geometry formulas!\n";
+  cout<< "Read through the menu below, type your choice and hit enter.\n";
+  cout<<  setw (5) << "\n 1.) Area of Circule \n";
+  cout<<  setw (5) << " 2.) Area of Rectangle \n";
+  cout<<  setw (5) << " 3.) Area of Triangle \n";
+  cout<<  setw (5) << " 4.) Quit Program \n";
+  cout<< "Enter Here:";
+  cin>> choice;
+
+  if (choice == 1)
+  {
+    const double pi = 3.14159;
+    cout<< "What is the radius of your circle?\n"
+      
+  }
+  
   
 }
