61599184f49ecb7b61bcb9d2a26f76ea3c55abea
Moon-Moon-Demadog <lmw2385@email.vccs.edu>
2025-07-05 16:11:05 +0000

    base code for shipping program

--- main.cpp
+++ main.cpp
@@ -1,93 +1,9 @@
-// This program will allow user to calculate basic geometry formulas.
+// This program is going to calculate shipping costs
 #include <iostream>
 #include <iomanip>
-#include <cmath>
 using namespace std;
 
-int main()
+int main ()
 {
-  int choice;
-  cout<< "Lets factor some Geometry formulas!\n";
-  cout<< "Read through the menu below, type your choice and hit enter.\n";
-  cout<<  setw (5) << "\n 1.) Area of Circule \n";
-  cout<<  setw (5) << " 2.) Area of Rectangle \n";
-  cout<<  setw (5) << " 3.) Area of Triangle \n";
-  cout<<  setw (5) << " 4.) Quit Program \n";
-  cout<< "Enter Here:";
-  cin>> choice;
-
-  if (choice == 1)
-  {
-   const double PI = 3.14159;
-    double radius;
-    double radius_squared;
-    double area;
-    cout<< "What is the radius of your circle?\n";
-      cout<< "Enter radius here :";
-    cin>> radius;
-    if (radius <= 0)
-    {
-    cout << "Enter a number greater than zero and try again." << endl;
-      return 1;
-    } else {
-       radius_squared = radius * radius;
-    area = PI * radius_squared;
-    cout<< "The area of the circle is: " << area << endl;
-    }
-  }
-  else if (choice == 2)
-  {
-    double Length, Width;
-    cout<< "What is the lenght of your rectangle? \n";
-      cin>> Length;
-    if (Length <= 0)
-    {
-      cout<< "Enter a number greater than zero, and try agian. \n";
-      return 2;
-    }
-    else {
-      cout<< "What is the width of you rectangle?\n";
-      cin>> Width;
-    }
-    if (Width <= 0)
-    {
-      cout<< "Enter a number greater than zero and try agian. \n)";
-      return 2;
-    } else {
-      double Area = Length * Width;
-      cout<< "The area of your rectangle is: " << Area << endl;
-    }
-  }
-  else if (choice == 3)
-  {
-    double base, height;
-    cout<< "What is the base of your triangle? \n?";
-    cin>> base;
-    if (base <= 0)
-    {
-      cout<< "Try again, enter a number greater than zero.\n";
-      return 3;
-    } else {
-      cout<< "Whate is the height of your triangle? \n";
-      cin>> height;
-    }
-    if (height <= 0)
-    {
-      cout<< "Try agian, enter a number greater than zero. \n";
-      return 3;
-    } else {
-      double area = base * height ;
-      cout<< "The area of your rectangle is; " << area << endl;
-    }
-  }
-  else if (choice < 1 || choice > 4)
-  {
-    cout<< "Invalid choice. Pleae enter a number between 1 and 4";
-    return 0;
-  }
-  else
-  {
-    cout<< "Okay, have a great day!" << endl;
-    return 0;
-  }
+  
 }
