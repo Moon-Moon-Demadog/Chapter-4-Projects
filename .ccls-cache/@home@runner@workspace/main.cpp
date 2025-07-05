c68adf61242f1c5d04d52d18fccf64129224ecbc
Moon-Moon-Demadog <lmw2385@email.vccs.edu>
2025-07-05 16:27:08 +0000

    Weights and Miles
    working on nested if/else if statements involving weight, need to add miles above... might delete and redo later.

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -5,5 +5,20 @@ using namespace std;
 
 int main ()
 {
+  long miles;
+  double weight;
+  cout<< "To accurately calculate your total charge to ship your package, I'll need some";
+  cout<< "information. :" << endl;
+  cout<< setw (5) << "In kilograms, how much does your package weigh:"<< endl;
+cin>> weight;  
+  if (weight <0 || weight >20)
+  {
+    cout<< "I'm sorry, Fast Freight Shipping limits package to 20kgs or less and they must weigh more than 0kgs." << endl;
+  } else if (weight >0 && weight <= 2) {
+    cout<< "Okay, we're shipping a package weighing: " << weight << " kgs."<< endl;}
+   else if (weight<6 && weight <2 ) {
+     cout<< "Okay, we're shipping a package weighing"
+   }
+    
   
 }
