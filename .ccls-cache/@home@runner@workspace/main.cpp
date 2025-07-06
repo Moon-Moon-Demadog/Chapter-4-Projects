1c79fd76d923df7df0a197d3833c02bd37fd666d
Moon-Moon-Demadog <lmw2385@email.vccs.edu>
2025-07-06 19:50:37 +0000

    logic error
    had logic error in my if statement, for months contains 31 days, had to rewrite it has if (month == 1 || month == 3 )etc. otherwise it was all calculating as true no matter what as an integer greater than 0 is true.

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -5,8 +5,7 @@ using namespace std;
 
 int main()
 {
-  int leapyear= 0, month, days = 0;
-  const int leapDays = 29;
+  int month, days = 0;
   const int divisor1 = 100;
   const int divisor400 = 400;
   const int divisor4 = 4;
@@ -27,9 +26,24 @@ int main()
   } else if ( year % divisor1 == 0 && year % divisor400 == 0){
     cout<< "That's a leap year!"<<  endl;
   } else if (year % divisor4 == 0){
-    cout<< "That's a leap year!"<< leapDays << endl;
+    cout<< "That's a leap year!"<< endl;
   } else {
-    cout<< "That's not a leap year!"<< days << leapyear << endl;
+    cout<< "That's not a leap year!"<<endl;
   }
+ if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+   days = 31;
+   
+ } else if (month == 2 && ((year % divisor4 == 0 && year % 100 != 0) || (year % divisor400 == 0))) {
+   days = 29;
+   
+ } else if (month == 2){
+   days = 28;
+   
+ } else if (month == 4 || 6 || 9 || 11){
+   days = 30;
+ }
+  cout<< setw (3) << "Days:   " << days << endl;
+  cout<< setw (3) << "Year:   " << year << endl;
+  
   return 0;
 }
